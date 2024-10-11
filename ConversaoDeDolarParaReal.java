2d4a2df1f96903eb0069a5392538f2816c926acf
Yasmin Ferrari Dias <158753555+YasminFerrariDias@users.noreply.github.com>
2024-10-10 21:08:35 -0300

    Add files via upload

---
+++ ConversaoDeDolarParaReal.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class ConversaoDeDolarParaReal {
+  public static void main(String[] args) {
+    Scanner var = new Scanner(System.in);
+    float din, dolar, conv;
+    System.out.print("Informe quantos dólares você possui: ");
+    din = var.nextFloat();
+    System.out.print("Informe a cotação do dólar: ");
+    dolar = var.nextFloat();
+    conv = din*dolar;
+    System.out.print("A conversão ficará para " + conv + " reais.");
+  }
+}
